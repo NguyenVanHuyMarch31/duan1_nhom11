161d3790f8e2831c96bfeb15162ff44c3713294d
Du an 1 <nguyenvanhuy3132005@gmail.com>
2024-11-11 07:55:31 +0700

    feat:giaodienAdmin

---
+++ commons/env.php
@@ -0,0 +1,16 @@
+<?php 
+
+// Biến môi trường, dùng chung toàn hệ thống
+// Khai báo dưới dạng HẰNG SỐ để không phải dùng $GLOBALS
+
+define('BASE_URL'       , 'http://du_an_1.test/duan1_Nhom11/');
+define('BASE_URL_ADMIN'       , 'http://du_an_1.test/duan1_Nhom11/admin/');
+
+
+define('DB_HOST'    , 'localhost');
+define('DB_PORT'    , 3306);
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME'    , 'web-ban-dong-ho');  // Tên database
+
+define('PATH_ROOT'    , __DIR__ . '/../');
