5311512cf38e8cc375985a12b3bec61905ea85f8
Du an 1 <nguyenvanhuy3132005@gmail.com>
2024-11-11 08:20:03 +0700

    feat:update

--- commons/env.php
+++ commons/env.php
@@ -3,14 +3,14 @@
 // Biến môi trường, dùng chung toàn hệ thống
 // Khai báo dưới dạng HẰNG SỐ để không phải dùng $GLOBALS
 
-define('BASE_URL'       , 'http://du_an_1.test/duan1_Nhom11/');
-define('BASE_URL_ADMIN'       , 'http://du_an_1.test/duan1_Nhom11/admin/');
+define('BASE_URL'       , 'http://localhost/duan1_nhom11/admin/');
+define('BASE_URL_ADMIN'       , 'http://localhost/duan1_nhom11/admin/admin/');
 
 
 define('DB_HOST'    , 'localhost');
 define('DB_PORT'    , 3306);
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_NAME'    , 'web-ban-dong-ho');  // Tên database
+define('DB_NAME'    , 'duan1_nhom11');  // Tên database
 
 define('PATH_ROOT'    , __DIR__ . '/../');
