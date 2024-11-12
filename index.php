377c50242de07bd2655a50a5b6954e770016710f
Du an 1 <nguyenvanhuy3132005@gmail.com>
2024-11-12 16:00:57 +0700

    feat:login_register

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+require_once './commons/env.php';
+require_once './commons/function.php';
+require_once './controllers/userController.php';
+require_once './models/userModel.php';
+
+$act = $_GET['act'] ?? '/';
+
+match ($act) {
+    '/' => (new userController())->trangchu(),
+    
+    'trangchu'  => (new userController())->trangchu(),
+    'dangky'     => (new userController())->postRegister(),
+    'dangnhap'   => (new userController())->postLogin(),
+    
+
+};
